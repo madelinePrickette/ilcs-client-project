f053f916a592c74a501102981f67d0c1ff5db82a
Madeline Prickette <madelineprickette@gmail.com>
2022-10-03 15:34:16 -0500

    rerouted EmployeeDashboard

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -14,7 +14,7 @@ import Footer from '../Footer/Footer';
 import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
 
 import AboutPage from '../AboutPage/AboutPage';
-import UserPage from '../UserPage/UserPage';
+import EmployeeDashboard from '../EmployeeDashboard/EmployeeDashboard';
 import InfoPage from '../InfoPage/InfoPage';
 import LandingPage from '../LandingPage/LandingPage';
 import LoginPage from '../LoginPage/LoginPage';
@@ -55,9 +55,9 @@ function App() {
           <ProtectedRoute
             // logged in shows UserPage else shows LoginPage
             exact
-            path="/user"
+            path="/EmployeeDashboard"
           >
-            <UserPage />
+            <EmployeeDashboard />
           </ProtectedRoute>
 
           <ProtectedRoute
@@ -75,7 +75,7 @@ function App() {
             {user.id ?
               // If the user is already logged in, 
               // redirect to the /user page
-              <Redirect to="/user" />
+              <Redirect to="/EmployeeDashboard" />
               :
               // Otherwise, show the login page
               <LoginPage />
@@ -89,7 +89,7 @@ function App() {
             {user.id ?
               // If the user is already logged in, 
               // redirect them to the /user page
-              <Redirect to="/user" />
+              <Redirect to="/EmployeeDashboard" />
               :
               // Otherwise, show the registration page
               <RegisterPage />
@@ -103,7 +103,7 @@ function App() {
             {user.id ?
               // If the user is already logged in, 
               // redirect them to the /user page
-              <Redirect to="/user" />
+              <Redirect to="/EmployeeDashboard" />
               :
               // Otherwise, show the Landing page
               <LandingPage />

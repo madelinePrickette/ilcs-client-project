07b11faada6cd4edfcf24fd865f75f3cd05676cb
Kelsey Kuno <kelsey.kuno@gmail.com>
2022-10-04 09:38:09 -0500

    post route works

---
+++ server/routes/adminAddClient.router.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+/**
+ * GET route template
+ */
+router.get('/', (req, res) => {
+  // GET route code here
+});
+
+/**
+ * POST route template
+ */
+router.post('/', (req, res) => {
+    console.log('req.body in post', req.body);
+    //make new client active by passing them 
+    const active = true;
+  queryText=`INSERT INTO "client" ("client_first_name", "client_last_name", "address", "city", "state", "zip", "bio", "client_active")
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8);`;
+  values = [
+    req.body.firstName,
+    req.body.lastName,
+    req.body.address,
+    req.body.city,
+    req.body.state,
+    req.body.zip,
+    req.body.bio,
+    active
+  ];
+
+  pool.query(queryText, values)
+    .then(response => {
+      res.sendStatus(200)
+    }).catch(err => {
+      console.log(err)
+      res.sendStatus(500)
+    })
+});
+
+module.exports = router;

fdef74e1565638d0054fa612a116f054b7e909e1
Matt Moua <matmoua@gmail.com>
2022-10-05 13:24:14 -0500

    form autopopulating and active checkbox working

--- server/routes/currentemployee.router.js
+++ server/routes/currentemployee.router.js
@@ -27,4 +27,40 @@ router.post('/',  rejectUnauthenticated, (req, res) => {
   // POST route code here
 });
 
+router.put('/:id', rejectUnauthenticated, (req, res) =>{
+  console.log(req.body.info.firstname)
+  const id = req.params.id;
+  const queryText = `
+    UPDATE "user" 
+    SET "first_name" = $1,
+      "last_name" = $2,
+      "username" = $3,
+      "clearance_level" = $4,
+      "email" = $5,
+      "pic" = $6,
+      "user_active" = $7
+    WHERE id = $8
+  ;`;
+
+  values = [
+    req.body.info.firstname,
+    req.body.info.lastname,
+    req.body.info.username,
+    req.body.info.clearancelevel,
+    req.body.info.email,
+    req.body.info.picture,
+    req.body.info.active,
+    id
+  ]
+
+  pool.query(queryText, values)
+    .then(response => {
+      res.sendStatus(200)
+    }).catch(error => {
+      console.log(error)
+      res.sendStatus(500)
+    })
+})
+
+
 module.exports = router;
--- src/redux/sagas/employeedetails.saga.js
+++ src/redux/sagas/employeedetails.saga.js
@@ -4,17 +4,25 @@ import { put, takeLatest, takeEvery } from 'redux-saga/effects';
 // worker Saga: will be fired on "FETCH_USER" actions
 function* fetchCurrentEmployee(action) {
   try {
-    console.log('fetching employee');
     const response = yield axios.get(`/api/currentemployee/${action.payload}`);
-    yield console.log('response is', response)
     yield put({ type: 'SET_CURRENT_EMPLOYEE', payload: response.data[0] });
   } catch (error) {
     console.log('Error in fetchEmployees saga', error);
   }
 }
 
+function* saveNewEmployeeInfo(action) {
+  try {
+    yield axios.put(`/api/currentemployee/${action.payload.employeeid}`, action.payload);
+    yield put({type: 'FETCH_CURRENT_EMPLOYEE', payload: action.payload.employeeid})
+  } catch (error) {
+      console.log('Error in saveNewEmployeeInfo', error)
+  }
+}
+
 function* employeeDetailsSaga() {
   yield takeEvery('FETCH_CURRENT_EMPLOYEE', fetchCurrentEmployee);
+  yield takeLatest('SAVE_NEW_EMPLOYEE_INFO', saveNewEmployeeInfo)
 }
 
 export default employeeDetailsSaga;

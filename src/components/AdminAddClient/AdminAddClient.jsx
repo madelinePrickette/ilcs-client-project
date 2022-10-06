ddfc64a2b474f8b647e13d0d849ea201609e8049
Kelsey Kuno <kelsey.kuno@gmail.com>
2022-10-06 08:43:41 -0500

    turned addclient into a component

--- src/components/AdminAddClient/AdminAddClient.jsx
+++ src/components/AdminAddClient/AdminAddClient.jsx
@@ -1,17 +1,34 @@
 import { useState } from 'react';
 import {useDispatch} from 'react-redux';
-import  {useHistory} from 'react-router-dom';
 
+//MUI Dialog imports
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 function AdminAddClient () {
 
     const dispatch = useDispatch();
-    const history = useHistory();
 
-    //client object
+    //new client object that takes in user form inputs
     const [newClient, setNewClient] = useState({firstName: '', lastName:'', address: '', city: '', state:'', zip:'', bio:''});
 
-    //setting input functions:
+    //Code that handles MUI dialog box open and close
+    const [open, setOpen] = useState(false);
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
+        setNewClient({firstName: '', lastName:'', address: '', city: '', state:'', zip:'', bio:''});
+
+    };
+
+    //setting user input functions:
     const handleFirstName = (event) => {
         setNewClient({...newClient, firstName: event.target.value})
     }
@@ -34,12 +51,6 @@ function AdminAddClient () {
         setNewClient({...newClient, bio: event.target.value})
     }
 
-    //cancel button route
-    const  handleCancel = () => {
-        history.push('/all-clients');
-    }
-    
-
     //handleSubmit form
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -50,99 +61,103 @@ function AdminAddClient () {
             payload: newClient
         })
         setNewClient({firstName: '', lastName:'', address: '', city: '', state:'', zip:'', bio:''});
-        history.push('/all-clients');
-
     }//end of handleSubmit
 
 
 
-    
-
     return(
         <>
-            <h2>Add New Client</h2>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    First Name
-                    <input
-                    type="text"
-                    placeholder="First Name"
-                    onChange={handleFirstName}
-                    value={newClient.firstName}
-                    required
+            <button onClick={handleClickOpen}>Add New Client</button>
+            {/* Add new client dialog */}
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>Add New Client</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Please enter information to add new client.
+                    </DialogContentText>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        label="First Name"
+                        type="text"
+                        fullWidth
+                        variant="standard"
+                        onChange={handleFirstName}
+                        value={newClient.firstName}
+                        required
                     />
-                </label>
-                <label>
-                    Last Name
-                    <input
-                    type="text"
-                    placeholder="Last Name"
-                    onChange={handleLastName}
-                    value={newClient.lastName}
-                    required
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        label="Last Name"
+                        type="text"
+                        fullWidth
+                        variant="standard"
+                        onChange={handleLastName}
+                        value={newClient.lastName}
+                        required
                     />
-                </label>
-                <label>
-                    Address
-                    <input
-                    type="text"
-                    placeholder="Address"
-                    onChange={handleAddress}
-                    value={newClient.address}
-                    required
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        label="address"
+                        type="text"
+                        fullWidth
+                        variant="standard"
+                        onChange={handleAddress}
+                        value={newClient.address}
+                        required
                     />
-                </label>
-                <label>
-                    City
-                    <input
-                    type="text"
-                    placeholder="City"
-                    onChange={handleCity}
-                    value={newClient.city}
-                    required
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        label="city"
+                        type="text"
+                        fullWidth
+                        variant="standard"
+                        onChange={handleCity}
+                        value={newClient.city}
+                        required
                     />
-                </label>
-                <label>
-                    State
-                    <input
-                    type="text"
-                    placeholder="State"
-                    onChange={handleState}
-                    value={newClient.state}
-                    required
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        label="state"
+                        type="text"
+                        fullWidth
+                        variant="standard"
+                        onChange={handleState}
+                        value={newClient.state}
+                        required
                     />
-                </label>
-                <label>
-                    Zip
-                    <input
-                    type="number"
-                    placeholder="Zip"
-                    onChange={handleZip}
-                    value={newClient.zip}
-                    required
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        label="Zip code"
+                        type="number"
+                        fullWidth
+                        variant="standard"
+                        onChange={handleZip}
+                        value={newClient.zip}
+                        required
                     />
-                </label>
-                <label>
-                    Bio
-                    <textarea 
-                    type="text"
-                    placeholder="Bio"
-                    onChange={handleBio}
-                    value={newClient.bio}
-                    required
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        label="bio"
+                        type="text"
+                        fullWidth
+                        variant="standard"
+                        onChange={handleBio}
+                        value={newClient.bio}
+                        required
                     />
-                </label>
-                <button
-                onClick={handleCancel}
-                >
-                    Cancel
-                </button>
-                <button
-                type="submit">
-                    Submit
-                </button>
-
-            </form>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Cancel</Button>
+                    <Button onClick={handleSubmit}>Add Client</Button>
+                </DialogActions>
+            </Dialog>
         </>
 
     );

ce1752f9bc57dd051eba97e5cc071e7a3c164d79
Matt Moua <matmoua@gmail.com>
2022-10-11 11:13:47 -0500

    comments for employee details added

--- src/components/EmployeeDetails/EmployeeDetails.jsx
+++ src/components/EmployeeDetails/EmployeeDetails.jsx
@@ -3,44 +3,56 @@ import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Button } from "@material-ui/core";
 
+//This component can be reached from the Admin side by click on an employee from viewing the employees list. The component displays an employee's name and current clients assigned to them. Contains two buttons, one to edit the employee's information and clients, and one to view all timesheets submitted by the current employee.
+
 function EmployeeDetails() {
+
+  useEffect(() => {
+    dispatch({ type: "FETCH_CURRENT_EMPLOYEE", payload: params.employeeid });
+    dispatch({
+      type: "FETCH_EMPLOYEE_CLIENT_LIST",
+      payload: params.employeeid,
+    });
+  }, []);
+
+  //Const variables
 
   const history = useHistory();
   const params = useParams();
   const dispatch = useDispatch();
   const employeeInfo = useSelector((store) => store.employeedetails);
-  const employeeClientList = useSelector(store => store.clientlist.employeeClientList);
+  const employeeClientList = useSelector(
+    (store) => store.clientlist.employeeClientList
+  );
+
+  //Const functions
 
   const clickEdit = () => {
-    history.push(`/editemployee/${params.employeeid}`)
-  }
-
-  useEffect(() => {
-    dispatch({ type: "FETCH_CURRENT_EMPLOYEE", payload: params.employeeid });
-    dispatch({ type: "FETCH_EMPLOYEE_CLIENT_LIST", payload: params.employeeid });
-  }, []);
+    history.push(`/editemployee/${params.employeeid}`);
+  };
 
   return (
     <div className="employeeDetailsDiv">
       <div key={employeeInfo.id}>
-        <img
-          className="profilePicDiv"
-          src={employeeInfo.pic}
-        />
+        <img className="profilePicDiv" src={employeeInfo.pic} />
         <div className="employeedetails">
-          <h2 style={{ width: "50%", marginLeft: "0px", marginRight: "auto"  }}>
+          <h2 style={{ width: "50%", marginLeft: "0px", marginRight: "auto" }}>
             {employeeInfo.first_name}
             {employeeInfo.last_name}
           </h2>
-          <Button onClick={clickEdit} variant="contained">Edit Employee</Button>
+          <Button onClick={clickEdit} variant="contained">
+            Edit Employee
+          </Button>
           <Button variant="contained">View All</Button>
         </div>
         <div className="clientListDiv">
           <h1>Clients</h1>
-          {employeeClientList.map(client => {
+          {employeeClientList.map((client) => {
             return (
-              <h3 key={client.client_id}>{client.client_first_name} {client.client_last_name}</h3>
-            )
+              <h3 key={client.client_id}>
+                {client.client_first_name} {client.client_last_name}
+              </h3>
+            );
           })}
         </div>
       </div>

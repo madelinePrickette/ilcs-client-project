1b1152769060f342c13c102b06958a94ea944e2f
Matt Moua <matmoua@gmail.com>
2022-10-12 14:21:10 -0500

    removed old table library, installed MaterialReactTable

--- src/components/AdminEmployeesView/AdminEmployeesView.jsx
+++ src/components/AdminEmployeesView/AdminEmployeesView.jsx
@@ -1,90 +1,74 @@
-import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { useHistory } from 'react-router-dom'
-import { Button, Link, Box, Paper } from '@material-ui/core'
-import MUIDataTable from "mui-datatables";
+import { useEffect, useMemo } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { Button, Link, Box, Paper, Typography } from "@material-ui/core";
+import MaterialReactTable from "material-react-table";
 
 function AdminEmployeesView() {
 
   useEffect(() => {
-    dispatch({type: 'FETCH_EMPLOYEES_LIST'});
+    dispatch({ type: "FETCH_EMPLOYEES_LIST" });
   }, []);
 
   //Const variables
 
   const dispatch = useDispatch();
-  const employeesList = useSelector(store => store.adminemployeesview);
+  const employeesList = useSelector((store) => store.adminemployeesview);
   const history = useHistory();
 
   const clickEmployee = (id) => {
-    // console.log(id);
-    history.push(`/employee/${id}`)
-
-  }
-
-  //Datatables variables
-  const columns = [
-    {
-      name: "",
-      label: "",
-      options: {
-       filter: false,
-       sort: false,
-      }
-     },
-     {
-      name: "first_name",
-      label: "First Name",
-      options: {
-       filter: true,
-       sort: true,
-      }
-     },
-     {
-      name: "last_name",
-      label: "Last Name",
-      options: {
-       filter: true,
-       sort: true,
-      }
-     },
-     {
-      name: "email",
-      label: "Email",
-      options: {
-       filter: true,
-       sort: true,
-      }
-     },
-     {
-      name: "id",
-      label: "ID",
-      options: {
-        display: false,
-       filter: true,
-       sort: true,
-      }
-     },
-  ]
-  const data = [];
-  const responsive = 'standard';
-  const options = {
-    selectableRowsHeader: false,
-    selectableRowsOnClick: false,
-    selectableRowsHideCheckboxes: true,
-    responsive: 'standard',
-    onRowClick: rowData => clickEmployee(rowData[4])
+    history.push(`/employee/${id}`);
   };
 
-
-
+  //MaterialReactTable variables
+  const data = [];
+  const columns = useMemo(
+    () => [
+      {
+        accessorKey: "first_name", //access nested data with dot notation
+        header: "First Name",
+        Cell: ({ cell, row }) => (
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              gap: "1rem",
+            }}
+          >
+            <img
+              alt="avatar"
+              height={50}
+              width={50}
+              src={row.original.pic}
+              loading="lazy"
+              style={{ borderRadius: "50%", objectFit: "cover" }}
+            />
+            <Typography>{cell.getValue()}</Typography>
+          </Box>
+        ),
+      },
+      {
+        accessorKey: "last_name",
+        header: "Last Name",
+      },
+      {
+        accessorKey: "email", //normal accessorKey
+        header: "Email",
+      },
+      {
+        accessorKey: "id", //normal accessorKey
+        header: "ID",
+      },
+    ],
+    []
+  );
 
   return (
     // <div className='adminEmployeesView'>
     //   <div className='employeeListTitle' style={{textAlign: 'center', width: '100%'}}>
     //     <h2>Employees</h2>
-    //     <Button 
-    //       variant='contained' 
+    //     <Button
+    //       variant='contained'
     //       style={{float: 'right', marginBottom: '10px'}}
     //       onClick={() => {history.push('/registration')}}
     //     >Add New Employee</Button>
@@ -102,18 +86,35 @@ function AdminEmployeesView() {
     //   )
     // })}
     // </div>
+
     <div>
-      {employeesList.map(employee => {
-        data.push([<img  style={{height: '75px', width: '75px', objectFit: 'cover'}} src={employee.pic}/>, employee.first_name, employee.last_name, employee.email, employee.id])
+      <div style={{ textAlign: "center" }}>
+        <h1>Employees</h1>
+      </div>
+      {employeesList.map((employee) => {
+        data.push({
+          pic: employee.pic,
+          first_name: employee.first_name,
+          last_name: employee.last_name,
+          email: employee.email,
+          id: employee.id,
+        });
       })}
-      <MUIDataTable
-      title={"Employees"}
-      data={data}
-      options={options}
-      columns={columns}
+      <MaterialReactTable
+        columns={columns}
+        data={data}
+        initialState={{ columnVisibility: { id: false } }}
+        muiTableBodyRowProps={({ row }) => ({
+          onClick: (event) => {
+            clickEmployee(row.original.id);
+          },
+          sx: {
+            cursor: "pointer", //you might want to change the cursor too when adding an onClick
+          },
+        })}
       />
     </div>
-  ) 
+  );
 }
 
 export default AdminEmployeesView;
